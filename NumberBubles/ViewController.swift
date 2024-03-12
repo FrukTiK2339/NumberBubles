f9022fd1018556538a5df9c4fa8a408e4556a33d
Dmitry Rybakov <d.rybakov@devplex.ru>
2024-03-12 15:16:23 +0300

    Init commit, base setup

--- NumberBubles/ViewController.swift
+++ NumberBubles/ViewController.swift
@@ -7,11 +7,80 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+fileprivate var cellReuseIdentifier = "BubleCollectionViewCellReuseIdentifier"
 
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    //MARK: - Private
+    private var collectionView: UICollectionView?
+    private var bubleArray = [[Int]]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupData()
+        setupCollectionView()
+    }
+    
+    private func setupData() {
+        for _ in 1...100 {
+            var cells = [Int]()
+            for _ in 1...Int.random(in: 10...15) {
+                cells.append(Int.random(in: 1..<100))
+            }
+            bubleArray.append(cells)
+        }
+    }
+    
+    private func setupCollectionView() {
+        let layout = UICollectionViewCompositionalLayout { section, _ -> NSCollectionLayoutSection? in
+            return self.layout(for: section)
+        }
+        let collectionView = UICollectionView(frame: view.bounds,collectionViewLayout: layout)
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellReuseIdentifier)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.showsVerticalScrollIndicator = false
+        view.addSubview(collectionView)
+        
+        self.collectionView = collectionView
+    }
+    
+    private func layout(for section: Int) -> NSCollectionLayoutSection {
+        let item = NSCollectionLayoutItem(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1),
+                heightDimension: .fractionalHeight(1)
+            )
+        )
+        item.contentInsets = NSDirectionalEdgeInsets(top: 8,
+                                                     leading: 8,
+                                                     bottom: 8,
+                                                     trailing: 8
+        )
+        let group = NSCollectionLayoutGroup.horizontal(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .estimated(80),
+                heightDimension: .estimated(80)),
+            subitems: [item])
+        
+        let sectionLayout = NSCollectionLayoutSection(group: group)
+        sectionLayout.orthogonalScrollingBehavior = .continuous
+        
+        return sectionLayout
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return bubleArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return bubleArray[section].count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellReuseIdentifier, for: indexPath)
+        cell.backgroundColor = .red
+        return cell
     }
 
 
